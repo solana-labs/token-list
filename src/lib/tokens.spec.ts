379e70383bbd6a2431a065059c4dbc6fac3c0af6
Josh Hundley <josh.hundley@gmail.com>
2021-03-08 19:42:17 -0800

    fix: make unit tests and eslint pass

--- src/lib/tokens.spec.ts
+++
@@ -1,8 +0,0 @@
-import test from 'ava';
-import { ENV, TOKENS } from './tokens';
-
-test('Token env is array', (t) => {
-  t.true(Array.isArray(TOKENS[ENV.MainnetBeta]));
-  t.true(Array.isArray(TOKENS[ENV.Testnet]));
-  t.true(Array.isArray(TOKENS[ENV.Devnet]));
-});
